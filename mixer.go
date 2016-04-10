fafacaec7b93fb30a618a763d39413200cdda920
coseyo <phpseyo@qq.com>
2016-04-10 17:09:22 +0800

    add some func

--- mixer.go
+++ mixer.go
@@ -1,6 +1,8 @@
 package datamixer
 
 import (
+	"errors"
+	"fmt"
 	"sync"
 )
 
@@ -8,17 +10,32 @@ type Mixer struct {
 	Datas []SourceData
 }
 
-func (m *Mixer) Mix() (dataRes DataResponse, err error) {
-	wg := sync.WaitGroup
+func (m *Mixer) GetData() (dataRes DataResponse, err error) {
+	var wg sync.WaitGroup
+	resps := []DataResponse{}
 
-	for _, sourceData := range m.Datas {
+	for _, sd := range m.Datas {
 		go func() {
 			wg.Add(1)
 			defer wg.Done()
 
-			sourceData.GetData()
+			sDataRes, sErr := sd.GetData(sd.Params, sd.Limit, sd.Offset)
+			if sErr != nil {
+				err = errors.New(fmt.Sprintf("%s %s", err.Error(), sErr.Error()))
+			}
+			resps = append(resps, sDataRes)
 		}()
 	}
 
 	wg.Wait()
+
+	if err != nil {
+		return
+	}
+
+	return m.mixResp(resps)
+}
+
+func (m *Mixer) mixResp(resps []DataResponse) (dataRes DataResponse, err error) {
+	return
 }
